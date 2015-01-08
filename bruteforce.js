3f9a2e692e371942d9e4bd2539e93646e91efc69
Andrew Laptev <a.d.laptev@gmail.com>
2015-01-08 04:34:04 +0000

    fromStringToNumber method

--- bruteforce.js
+++ bruteforce.js
@@ -1,4 +1,20 @@
 var Brute = function(options) {
+    options = options || {};
     this.characterSet = options.characterSet || ['a', 'b', 'c', 'd', 'e', 'f'];
-    this.endPoint = options.endPoint || []
+    this.length = this.characterSet.length;
+    this.endPoint = options.endPoint || [];
 };
+
+Brute.prototype.fromStringToNum = function(string) {
+    var s = string.split(''), l = s.length, num = 0;
+    for (var i = l; i--; ) {
+        var indexOf = this.characterSet.indexOf(s[i]);
+        if (!~indexOf) {
+            throw new Error('unknown character `' + s[i] + '` at position ' + i);
+        }
+        num += (indexOf + 1) * Math.pow(this.length,  l - i - 1);
+    }
+    return num;
+};
+
+var b = new Brute();
