03fefc8e10591576bf4de9f163408880f058707a
Andrew Laptev <a.d.laptev@gmail.com>
2015-01-09 07:56:34 +0000

    last commit

--- bruteforce.js
+++ bruteforce.js
@@ -2,7 +2,8 @@ var Brute = function(options) {
     options = options || {};
     this.characterSet = options.characterSet || ['a', 'b', 'c', 'd', 'e', 'f'];
     this.length = this.characterSet.length;
-    this.endPoint = options.endPoint || [];
+    this.endPoint = options.endPoint || 1;
+    this.async = 5;
 };
 
 /**
@@ -23,8 +24,28 @@ Brute.prototype.fromStringToNumber = function(string) {
 };
 
 /**
- * Returns string based on the number and character tab
+ * Returns string based on the number and character table
  */
 Brute.prototype.fromNumberToString = function(number) {
     return (number > 0) ? this.fromNumberToString(Math.floor((number - 1) / this.length)) + this.characterSet[(number - 1) % this.length] : ''; 
 };
+
+Brute.prototype.start = function() {
+    
+};
+
+Brute.prototype.exec = function(string, callback) {
+    console.log(string);
+    callback(null);
+};
+
+Brute.prototype.save = function(callback) {
+    require('fs').writeFile('.bf', this.endPoint, callback);
+};
+
+Brute.prototype.next = function* () {
+    yield this.fromNumberToString(this.endPoint++);
+};
+
+var b = new Brute();
+console.log(b.next().next());
